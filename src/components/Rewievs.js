c803caea899b8abd65a277574122573f9734cde7
FTMustafa <mustafamustafatw@gmail.com>
2024-10-26 05:12:15 +0300

    Değerlendirme güncellemesi
    basic değerlendirme cardları oluşturdum ve anasayfaya ekledim + ufak tefek düzenlemeler.

---
+++ src/components/Rewievs.js
@@ -0,0 +1,60 @@
+const Rewievs = [
+    {
+        id: 1,
+        foto: '',
+        aciklama: 'Bu bir açıklamadır',
+        rating: 5,
+        yorumlar: [
+            {
+                k_id: 1,
+                k_foto: '',
+                k_yorum: 'Bu bir yorumdur 1',
+            },
+            {
+                k_id: 2,
+                k_foto: '',
+                k_yorum: 'Bu bir yorumdur 2',
+            },
+            {
+                k_id: 3,
+                k_foto: '',
+                k_yorum: 'Bu bir yorumdur 3',
+            },
+            {
+                k_id: 3,
+                k_foto: '',
+                k_yorum: 'Bu bir yorumdur 4',
+            },
+        ]
+    },
+    {
+        id: 2,
+        foto: '',
+        aciklama: 'Bu bir açıklamadır',
+        rating: 5,
+        yorumlar: [
+            {
+                k_id: 1,
+                k_foto: '',
+                k_yorum: 'Bu bir yorumdur 1',
+            },
+            {
+                k_id: 2,
+                k_foto: '',
+                k_yorum: 'Bu bir yorumdur 2',
+            },
+            {
+                k_id: 3,
+                k_foto: '',
+                k_yorum: 'Bu bir yorumdur 3',
+            },
+            {
+                k_id: 3,
+                k_foto: '',
+                k_yorum: 'Bu bir yorumdur 4',
+            },
+        ]
+    }
+]
+
+export default Rewievs;
--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -1,11 +1,30 @@
-import './Main.css';
+import "./Main.css";
+import Rewievs from "../Rewievs";
 
-function Main(){
-    return(
-        <div className='main'>
-            Anasayfa
-        </div>
-    );
+function Main() {
+  return (
+    <div className="main">
+      <div className="main-left">
+        {Rewievs.map((item) => (
+          <div key={item.id} className="reviews">
+            <img src={item.foto} />
+            <div className="reviews-mid">
+              <p>{item.aciklama}</p>
+              <p>{item.rating}</p>
+            </div>
+            <div className="reviews-right">
+              {item.yorumlar.map((yorum) => (
+                    <div key={yorum.k_id} className="reviews-comments">
+                      <p>{yorum.k_yorum}</p>
+                    </div>
+                  ))}
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="main-right"></div>
+    </div>
+  );
 }
 
 export default Main;
